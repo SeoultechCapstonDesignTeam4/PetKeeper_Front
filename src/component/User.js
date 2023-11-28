77d271375c7da5025d8666cedd1a41cc6720bfb9
hchbae1001 <hchbae1001@gmail.com>
2023-11-28 13:44:35 +0900

    회원 삭제기능 구현

--- src/component/User.js
+++ src/component/User.js
@@ -1,4 +1,27 @@
+import secureLocalStorage from "react-secure-storage";
+import createAxiosInstance from "./createAxiosInstance";
+import { useParams } from "react-router-dom";
+function deleteUser(id) {
+  const axiosInstance = createAxiosInstance();
+  axiosInstance.delete(`/user/${id}`)
+    .then((response) => {
+      console.log(response);
+      alert('회원 삭제 완료');
+      window.location.href = '/userList';
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('회원 삭제 실패');
+    });
+}
+
 function User({ user }) {
+  const { id } = useParams();
+
+  const handleDeleteClick = () => {
+    deleteUser(id);
+  };
+
   return (
     <div className="user-container">
       <h2>User Information</h2>
@@ -6,17 +29,15 @@ function User({ user }) {
         <p>
           <strong>ID:</strong> {user.USER_ID}
         </p>
-
         <div className="user-image-container">
-        <img
-          src={user.USER_IMAGE}
-          alt="User's Profile"
-          width={200}
-          height={200}
-          className="user-image"
-        />
-      </div>
-
+          <img
+            src={user.USER_IMAGE}
+            alt="User's Profile"
+            width={200}
+            height={200}
+            className="user-image"
+          />
+        </div>
         <p>
           <strong>Email:</strong> {user.USER_EMAIL}
         </p>
@@ -30,7 +51,11 @@ function User({ user }) {
           <strong>Authorization:</strong> {user.USER_AUTH}
         </p>
       </div>
-
+      <div>
+        {secureLocalStorage.getItem('USER_AUTH') === 'admin' && (
+          <input type="button" value="회원 삭제" onClick={handleDeleteClick} />
+        )}
+      </div>
     </div>
   );
 }
